cdc6a0a0fe7bf25f6922a63244cc3118c84a946e
hristo-vrigazov <hvrigazov@gmail.com>
2017-01-13 10:39:38 +0200

    Scripts for splitting the data

--- data/sample/copy_images.py
+++ data/sample/copy_images.py
@@ -1,18 +1,37 @@
+import sys
+import os
 import pandas as pd
+
 from shutil import copyfile
 
-driving_log = pd.read_csv('driving_log.csv')
 
-def source_image_filename(filename):
-	return '../{}'.format(filename)
+# IMG_FOLDER is a folder that contains IMG folder, not th
+# folder itself
+
+def source_image_filename(source_img_folder, filename):
+	return '{0}/{1}'.format(source_img_folder, filename)
 
 
-def copy_images_from_perspective(perspective):
+def copy_images_from_perspective(source_img_folder, destination_img_folder, driving_log, perspective):
 	for image_file in driving_log[perspective]:
 		image_file = image_file.lstrip().rstrip()
-		copyfile(source_image_filename(image_file), image_file)
+		output_image_file = '{0}/{1}'.format(destination_img_folder, image_file)
+		copyfile(source_image_filename(source_img_folder, image_file), output_image_file)
 
-perspectives = ['center', 'left', 'right']
 
-for perspective in perspectives:
-	copy_images_from_perspective(perspective)
+def copy_images(SRC_IMG_FOLDER, DEST_IMG_FOLDER, PATH_TO_DRIVING_LOG):
+	driving_log = pd.read_csv(PATH_TO_DRIVING_LOG)
+
+	perspectives = ['center', 'left', 'right']
+
+	if not os.path.exists('{}/IMG'.format(DEST_IMG_FOLDER)):
+	    os.makedirs('{}/IMG'.format(DEST_IMG_FOLDER))
+
+	for perspective in perspectives:
+		copy_images_from_perspective(SRC_IMG_FOLDER, DEST_IMG_FOLDER, driving_log, perspective)
+
+if __name__ == "__main__":
+	IMG_FOLDER = '..' if not len(sys.argv) >= 2 else sys.argv[1]
+	DEST_IMG_FOLDER = '.' if not len(sys.argv) >= 3 else sys.argv[2]
+	PATH_TO_DRIVING_LOG = 'driving_log.csv' if not len(sys.argv) >= 4 else sys.argv[3]
+	copy_images(IMG_FOLDER, DEST_IMG_FOLDER, PATH_TO_DRIVING_LOG)
---
+++ data/sample/train_validation_test_split.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import sys
+import os
+
+from sklearn.model_selection import train_test_split
+from copy_images import copy_images
+
+PATH_TO_DATA = '.' if not len(sys.argv) == 2 else sys.argv[1]
+
+IMG_FOLDER = '{}/IMG'.format(PATH_TO_DATA)
+PATH_TO_DRIVING_LOG = '{}/driving_log.csv'.format(PATH_TO_DATA)
+
+driving_log_df = pd.read_csv(PATH_TO_DRIVING_LOG)
+
+train_df, test_df =  train_test_split(driving_log_df, test_size = 0.2)
+
+train_df, validation_df = train_test_split(train_df, test_size = 0.2)
+
+print(len(train_df))
+print(len(test_df))
+print(len(validation_df))
+
+dataframes = {'train': train_df, 'test': test_df, 'valid': validation_df}
+
+folders = {}
+folders['train'] = '{}/train'.format(PATH_TO_DATA)
+folders['test'] = '{}/test'.format(PATH_TO_DATA)
+folders['valid'] = '{}/valid'.format(PATH_TO_DATA)
+
+def create_folder_if_does_not_exist(folder):
+	if not os.path.exists(folder):
+	    os.makedirs(folder)
+
+for dataset in folders:
+	create_folder_if_does_not_exist(dataset)
+	path_to_driving_log_dataset = '{}/driving_log.csv'.format(folders[dataset])
+	dataframes[dataset].to_csv(path_to_driving_log_dataset)
+	copy_images(PATH_TO_DATA, folders[dataset], path_to_driving_log_dataset)
