5a568bdd0ec8d52cef614d50afe148cdd560e1f8
hristo-vrigazov <hvrigazov@gmail.com>
2017-01-25 20:05:47 +0200

    Change download script to use train, validation and test data

--- download.sh
+++ download.sh
@@ -1,2 +1,8 @@
-wget https://www.dropbox.com/s/p8vltf43acxpm16/all_data.zip &&
-unzip all_data.zip
+wget https://www.dropbox.com/s/82c73qd8ls29boi/train.zip &&
+unzip train.zip &&
+wget https://www.dropbox.com/s/ege1kxpuhritqd2/test.zip &&
+unzip test.zip &&
+wget https://www.dropbox.com/s/w6yuizp6yt382pk/valid.zip &&
+unzip valid.zip &&
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip &&
+unzip data.zip
