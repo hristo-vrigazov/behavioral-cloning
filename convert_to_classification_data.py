222d2a5a86ae380c938f8c93c76a2b6009a952dc
hristo-vrigazov <hvrigazov@gmail.com>
2017-01-26 22:17:30 +0200

    Added some utils

---
+++ convert_to_classification_data.py
@@ -0,0 +1,6 @@
+import sys
+from utils import convert_data_to_classification
+
+data_folder = sys.argv[1]
+
+convert_data_to_classification(data_folder)
---
+++ utils.py
@@ -0,0 +1,29 @@
+import sys
+import pandas as pd
+import numpy as np
+
+def img_folder(data_folder):
+    return '{}/IMG'.format(data_folder)
+
+
+def path_driving_log(data_folder):
+    return '{}/driving_log.csv'.format(data_folder)
+
+
+def get_driving_log_dataframe(data_folder):
+    driving_log_df = pd.read_csv(path_driving_log(data_folder))
+    return driving_log_df
+
+
+def get_steering_df_in_range(df, start, end):
+	return df[df['steering'] >= start][df['steering'] <= end]
+
+def convert_data_to_classification(data_folder):
+	driving_log_df = get_driving_log_dataframe(data_folder)
+	images_folder = img_folder(data_folder)
+	class_ranges = np.linspace(-1, 1, 20)
+
+	for first, second in zip(class_ranges, class_ranges[1:]):
+		df_steering_in_range = get_steering_df_in_range(driving_log_df, first, second)
+		print(len(df_steering_in_range))
+
