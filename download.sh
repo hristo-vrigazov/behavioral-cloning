734ee8a241adc916cc7d3bf6bb73c02a8352cf46
hristo-vrigazov <hvrigazov@gmail.com>
2017-01-26 15:42:23 +0200

    Change links to the fixed dataset

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
-wget https://www.dropbox.com/s/82c73qd8ls29boi/train.zip &&
+wget https://www.dropbox.com/s/w2iy0wjuyeb1l0k/train.zip &&
 unzip train.zip &&
-wget https://www.dropbox.com/s/ege1kxpuhritqd2/test.zip &&
+wget https://www.dropbox.com/s/3p2uugyrnmt166l/test.zip &&
 unzip test.zip &&
-wget https://www.dropbox.com/s/w6yuizp6yt382pk/valid.zip &&
+wget https://www.dropbox.com/s/3wucwt0c6r57jj0/valid.zip &&
 unzip valid.zip &&
 wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip &&
 unzip data.zip
