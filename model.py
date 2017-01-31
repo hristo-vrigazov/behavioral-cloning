cd3aa3642745acaf03a155e8c59d58cc97e96759
Hristo Vrigazov <hvrigazov@gmail.com>
2017-01-31 17:42:06 +0000

    CHange image size to original NVIDIA paper

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ from keras.models import load_model
 pipeline = NvidiaPipeLine()
 
 BATCH_SIZE = 16
-EPOCHS = 5
+EPOCHS = 1
 
 def preprocess(image):
     return pipeline.preprocess_image(image)
--- models/nvidia_pipeline.py
+++ models/nvidia_pipeline.py
@@ -5,6 +5,7 @@ from keras.layers import Convolution2D
 from keras.layers import Flatten
 from keras.layers import Dropout
 from keras.layers import Lambda
+from keras.layers import Activation
 from keras.layers.advanced_activations import ELU
 
 from .abstract_pipeline import AbstractPipeline
@@ -16,8 +17,8 @@ import numpy as np
 class NvidiaPipeLine(AbstractPipeline):
 
     def __init__(self):
-        self.input_shape = (90, 320, 3)
-        self.input_resize_to = (320, 90)
+        self.input_shape = (66, 200, 3)
+        self.input_resize_to = (200, 66)
 
 
     def get_train_samples(self, df):
@@ -54,49 +55,32 @@ class NvidiaPipeLine(AbstractPipeline):
     def get_model(self):
         model = Sequential()
 
-        regularization_coef = 10e-8
+        model.add(Lambda(lambda x: x/255 - .5, input_shape=self.input_shape))
+        
+        model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode='valid'))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode='valid'))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid'))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='valid'))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='valid'))
 
-        model.add(Lambda(lambda x: x/255.0,
-                    input_shape=self.input_shape))
-        model.add(Convolution2D(24, 
-                                5, 5,
-                               subsample=(2, 2),
-                               init='he_normal'))
-        model.add(ELU())
-        model.add(Convolution2D(36, 
-                                5, 5,
-                               subsample=(2, 2),
-                               init='he_normal'))
-        model.add(ELU())
-        model.add(BatchNormalization())
-        model.add(Convolution2D(48, 
-                                5, 5,
-                               subsample=(2, 2),
-                               init='he_normal'))
-        model.add(ELU())
-        model.add(BatchNormalization())
-        model.add(Dropout(0.2))
-        model.add(Convolution2D(64,
-                               3, 3,
-                               init='he_normal'))
-        model.add(ELU())
-        model.add(BatchNormalization())
-        model.add(Dropout(0.3))
-        model.add(Convolution2D(64,
-                               3, 3,
-                               init='he_normal'))
-        model.add(ELU())
-        model.add(BatchNormalization())
-        model.add(Dropout(0.4))
         model.add(Flatten())
-        model.add(Dense(100, activation='relu', init='he_normal'))
-        model.add(BatchNormalization())
-        model.add(Dense(50, activation='relu', init='he_normal'))
-        model.add(BatchNormalization())
-        model.add(Dense(10, activation='relu', init='he_normal'))
-        model.add(Dense(1, init='he_normal'))
+        model.add(Dropout(.5))
+        model.add(Activation('relu'))
+
+        model.add(Dense(100))
+        model.add(Activation('relu'))
+
+        model.add(Dense(50))
+        model.add(Activation('relu'))
+
+        model.add(Dense(10))
+        model.add(Activation('relu'))
+        
+        model.add(Dense(1))
 
         return model
-        
-   
-
+      
