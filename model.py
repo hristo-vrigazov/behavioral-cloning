f97a1ff2c8d40c9604c1b206dd9e59fdba6baf38
hristo-vrigazov <hvrigazov@gmail.com>
2017-02-02 22:59:54 +0200

    Add some comments in model.py

--- model.py
+++ model.py
@@ -13,6 +13,7 @@ from PIL import Image
 from utils import get_driving_log_dataframe
 from utils import get_callbacks
 from keras.models import load_model
+
 # Instead of implementing here, the model is in the
 # models/ directory, because this allows to quickly
 # switch between different pipelines
@@ -21,50 +22,15 @@ pipeline = NvidiaPipeLine()
 BATCH_SIZE = 32
 EPOCHS = 2
 
+# this function is also used in drive.py,
+# this way when switching the pipeline the 
+# preprocessing for driving is also changed
+# appropriately
 def preprocess(image):
     return pipeline.preprocess_image(image)
 
 
-def get_preprocessed_dataframe(data_folder, batch_size=64):
-    angles_df = pd.read_csv('{}/angles.csv'.format(data_folder), header=None)
-    angles_df = angles_df.reindex(np.random.permutation(angles_df.index))
-
-    number_of_examples = len(angles_df)
-
-    while True:
-        image_series = angles_df[0]
-        steering_series = angles_df[1]
-        for offset in range(0, number_of_examples, batch_size):
-            X_train = []
-            y_train = []
-            weights = []
-
-            end_of_batch = min(number_of_examples, offset + batch_size)
-
-            for j in range(offset, end_of_batch):
-               image_filename = image_series[j].lstrip().rstrip()
-                    
-               image = Image.open('{0}/{1}'.format(data_folder, image_filename))
-               image_np = preprocess(image)
-               label = steering_series[j]
-                    
-               X_train.append(image_np)
-               y_train.append(label)
-               weights.append(1)
-                    
-               flipped_image = np.fliplr(image_np)
-               flipped_label = -label
-                    
-               X_train.append(flipped_image) 
-               y_train.append(flipped_label)
-               weights.append(1)
-                
-                    
-               X_train, y_train, weights = shuffle(X_train, y_train, weights)
-               yield np.array(X_train), np.array(y_train), np.array(weights)
-
-
-def train(data_folder, validation_folder, restart_model_path=None, train_generator=None):
+def train(data_folder, validation_folder, restart_model_path=None):
     if restart_model_path:
         model = load_model(restart_model_path)
         print("Using existing model")
@@ -75,14 +41,15 @@ def train(data_folder, validation_folder, restart_model_path=None, train_generat
 
     samples = pipeline.get_train_samples(get_driving_log_dataframe(data_folder))
 
-    if not train_generator:
-        train_generator = pipeline.get_train_generator(data_folder, batch_size=BATCH_SIZE)
+    train_generator = pipeline.get_train_generator(data_folder, batch_size=BATCH_SIZE)
 
     model.summary()
 
     image_generator = train_generator
     validation_generator = pipeline.get_validation_generator(validation_folder, batch_size=BATCH_SIZE)
     nb_val_samples = pipeline.get_validation_samples(get_driving_log_dataframe(validation_folder))
+
+    # callbacks that save weights after each epoch
     callbacks_list = get_callbacks()
 
     model.fit_generator(image_generator, 
@@ -92,6 +59,7 @@ def train(data_folder, validation_folder, restart_model_path=None, train_generat
                        validation_data=validation_generator,
                        nb_val_samples=nb_val_samples)
 
+    # save everything for possible finetuning in the future
     model.save('model-compiled.h5')
 
     json_string = model.to_json()
