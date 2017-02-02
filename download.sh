38368b8a4c453f5b71d3fff39a8e7dbc1126fba4
hristo-vrigazov <hvrigazov@gmail.com>
2017-02-02 23:15:16 +0200

    Change download.sh file

--- download.sh
+++ download.sh
@@ -1,4 +1,6 @@
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip &&
+unzip data.zip &&
 wget https://www.dropbox.com/s/0x1t9vc76tba77s/valid.zip &&
 unzip valid.zip &&
-wget https://www.dropbox.com/s/3l7d0gt46l3ppvy/smooth.zip &&
-unzip smooth.zip
+wget https://www.dropbox.com/s/nl6r49zg6jdjr8p/recover.zip &&
+unzip recover
